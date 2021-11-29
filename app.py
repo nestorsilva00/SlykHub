a89f6ad01dae56845bed567c00a69b60239bb1d3
nestorsilva00 <nestorsilvahernandez00@gmail.com>
2021-11-29 16:16:24 -0500

    updating requirements txt again

--- app.py
+++ app.py
@@ -2,19 +2,8 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 import logic as App_logic
 app = Flask(__name__)
 
-@app.route('/')
-def users():
-    assets = None
-    return render_template('users.html', assets=assets)
 
-@app.route('/update_apikey', methods=['GET', 'POST'])
-def update_users():
-    apikey = request.form['apikey']
-    data = App_logic.get_users_data(apikey)
-    assets = App_logic.get_wallets_balance(apikey)
-    return render_template('users.html', user_data=data, assets=assets)
-
-@app.route('/users', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def users_new_table():
     assets = None
     return render_template('table.html', assets=assets)
